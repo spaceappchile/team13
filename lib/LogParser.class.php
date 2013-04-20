03f041733db507cb8b7cc4953f2eb176a2ada67c
Alvaro Véliz <alvaro.veliz@gmail.com>
2013-04-20 15:32:45 -0300

    Parsing this shit 1313

--- lib/LogParser.class.php
+++ lib/LogParser.class.php
@@ -13,41 +13,38 @@ class LogParser
   public function __construct($filename)
   {
     $this->filename = $filename;
-    $this->parser = xml_parser_create('UTF-8');
-
-    xml_parser_set_option($this->parser, XML_OPTION_SKIP_WHITE, true);
-    xml_parser_set_option($this->parser, XML_OPTION_TARGET_ENCODING, 'UTF-8');
-    xml_set_object($this->parser, $this);
-    xml_set_element_handler($this->parser, 'startElement', 'endElement');
-    xml_set_character_data_handler($this->parser, 'characterData');
   }
 
   public function parse()
   {
     $fp = fopen($this->filename, 'r');
     $iterations = 0;
-    $max_iterations = 7;
-    $line1 = '<Debug TimeStamp="2013-04-17T23:51:25.349" File="ACDImpl.cpp" Line="643" Routine="ReceiverBandMod::ReceiverBand ACDImpl::getCurrentCartrigeInternal()" Host="" Process="CONTROL/DV14/cppContainer" Thread="CONTROL/DV14/FrontEnd/ACDMonitoringThread" Context="" SourceObject="CONTROL/DV14/cppContainer-GL"><![CDATA[Problem manipulating hardware. (type=10000, code=21)]]><Data Name="Additional"><![CDATA[Could not determine the Calibrationd device position. It returned: 14]]></Data></Debug>';
+    $max_iterations = 100;
 
-    $line2 = '<Debug TimeStamp="2013-04-17T23:51:25.349" File="FrontEndUtils.cpp" Line="118" Routine="ReceiverBandMod::ReceiverBand FrontEndUtils::intToBand(int)" Host="" Process="CONTROL/DV14/cppContainer" Thread="CONTROL/DV14/FrontEnd/ACDMonitoringThread" Context="" SourceObject="CONTROL/DV14/cppContainer-GL"><![CDATA[Illegal Parameter Error (type=10000, code=2)]]><Data Name="Additional"><![CDATA[The requested band number is unrecognized]]></Data></Debug>';
-
-    $line3 = '<Debug TimeStamp="2013-04-17T23:51:25.398" File="ACDImpl.cpp" Line="643" Routine="ReceiverBandMod::ReceiverBand ACDImpl::getCurrentCartrigeInternal()" Host="" Process="CONTROL/DV14/cppContainer" Thread="CONTROL/DV14/FrontEnd/ACDMonitoringThread" Context="" SourceObject="CONTROL/DV14/cppContainer-GL"><![CDATA[Problem manipulating hardware. (type=10000, code=21)]]><Data Name="Additional"><![CDATA[Could not determine the Calibrationd device position. It returned: 14]]></Data></Debug>
-    ';
-
-    while( $line = fgets($fp) ) {
+    while( $line = fgets($fp) ) 
+    {
       $line = trim($line);
 
       if ($iterations > 3) {
+        $this->parser = xml_parser_create('UTF-8');
+
+        xml_parser_set_option($this->parser, XML_OPTION_SKIP_WHITE, true);
+        xml_parser_set_option($this->parser, XML_OPTION_TARGET_ENCODING, 'UTF-8');
+        xml_set_object($this->parser, $this);
+        xml_set_element_handler($this->parser, 'startElement', 'endElement');
+        xml_set_character_data_handler($this->parser, 'characterData');
+
         $this->parseLine($line, $iterations);
+
+        xml_parser_free($this->parser);
       }  
 
       $iterations++;
 
-      if ($iterations == $max_iterations) {
-        break;
-      }
+      // if ($iterations == $max_iterations) {
+      //   break;
+      // }
     }
-    xml_parser_free($this->parser);
     fclose($fp);
   }
 
@@ -74,9 +71,9 @@ class LogParser
   function parseLine( $line , $iteration = 0 )
   {
     echo htmlentities($line) . '<br />'; 
-    xml_parse($this->parser, $line) or die(sprintf('XML ERROR: %s at line %d (Code : %d) - Iteration : %d',
+    xml_parse($this->parser, $line) or die(sprintf('XML ERROR: %s at line %d column %d (Code : %d)',
          xml_error_string(xml_get_error_code($this->parser)),
-         xml_get_current_line_number($this->parser), xml_get_error_code($this->parser), $iteration));
+         xml_get_current_line_number($this->parser), xml_get_current_column_number($this->parser), xml_get_error_code($this->parser)));
     echo '<hr />';
   }
 }
