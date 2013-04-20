f7ec9eeced61724ea5ac66724a6b5bb165c41298
Alvaro Véliz <alvaro.veliz@gmail.com>
2013-04-20 16:23:28 -0300

    Parsing success

--- lib/LogParser.class.php
+++ lib/LogParser.class.php
@@ -19,24 +19,34 @@ class LogParser
   {
     $fp = fopen($this->filename, 'r');
     $iterations = 0;
-    $max_iterations = 100;
+    $max_iterations = 10000;
+    $previousLine = '';
 
     while( $line = fgets($fp) ) 
     {
       $line = trim($line);
 
       if ($iterations > 3) {
-        $this->parser = xml_parser_create('UTF-8');
 
-        xml_parser_set_option($this->parser, XML_OPTION_SKIP_WHITE, true);
-        xml_parser_set_option($this->parser, XML_OPTION_TARGET_ENCODING, 'UTF-8');
-        xml_set_object($this->parser, $this);
-        xml_set_element_handler($this->parser, 'startElement', 'endElement');
-        xml_set_character_data_handler($this->parser, 'characterData');
+        if ( preg_match('/(.+)>$/i', $line)) {
+          $this->parser = xml_parser_create('UTF-8');
 
-        $this->parseLine($line, $iterations);
+          xml_parser_set_option($this->parser, XML_OPTION_SKIP_WHITE, true);
+          xml_parser_set_option($this->parser, XML_OPTION_TARGET_ENCODING, 'UTF-8');
+          xml_set_object($this->parser, $this);
+          xml_set_element_handler($this->parser, 'startElement', 'endElement');
+          xml_set_character_data_handler($this->parser, 'characterData');
 
-        xml_parser_free($this->parser);
+          $line = $previousLine . $line;
+          $this->parseLine($line, $iterations);
+
+          xml_parser_free($this->parser);
+          $this->parser = NULL; 
+          $previousLine = '';
+        }
+        else {
+          $previousLine .= $line;
+        }
       }  
 
       $iterations++;
@@ -70,10 +80,9 @@ class LogParser
 
   function parseLine( $line , $iteration = 0 )
   {
-    echo htmlentities($line) . '<br />'; 
-    xml_parse($this->parser, $line) or die(sprintf('XML ERROR: %s at line %d column %d (Code : %d)',
+    xml_parse($this->parser, $line) or die(sprintf('XML ERROR: %s at line %d column %d byte %d (Code : %d) - Iteration : %d<br />Line : %s',
          xml_error_string(xml_get_error_code($this->parser)),
-         xml_get_current_line_number($this->parser), xml_get_current_column_number($this->parser), xml_get_error_code($this->parser)));
+         xml_get_current_line_number($this->parser), xml_get_current_column_number($this->parser), xml_get_current_byte_index($this->parser), xml_get_error_code($this->parser), $iteration, htmlentities($line)));
     echo '<hr />';
   }
 }
